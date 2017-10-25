43380563c52b7ba4637a613a61c24c91c99476ab
sahildorwat <sahilcool.dorwat02@gmail.com>
2017-10-24 22:01:39 -0400

    course updated

--- src/bean/Course.java
+++ src/bean/Course.java
@@ -1,12 +1,28 @@
 package bean;
-
+import java.util.Date;
 public class Course {
-	Integer id;
+	String id;
 	String name;
-	public Integer getId() {
+	Date start_date;
+	Date end_date;
+	
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	
+	public String getId() {
 		return id;
 	}
-	public void setId(Integer id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 	public String getName() {
@@ -17,7 +33,9 @@ public class Course {
 	}
 	@Override
 	public String toString() {
-		return "Course [id=" + id + ", name=" + name + "]";
+		return "Course [id=" + id + ", name=" + name + ", start_date="
+				+ start_date + ", end_date=" + end_date + "]";
 	}
+	
 	
 }
