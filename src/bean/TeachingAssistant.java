b26722524f3d45dd070fc2df2bf8724881d3abec
krkulkar <kunalkul1@gmail.com>
2017-10-27 15:21:48 -0400

    Initial code for TA

--- src/bean/TeachingAssistant.java
+++ src/bean/TeachingAssistant.java
@@ -12,7 +12,38 @@ public class TeachingAssistant extends Student{
 			this.name=rs.getString("name");
 			this.user_id=rs.getString("user_id");
 			this.password=rs.getString("password");
-			
+			while(true) {
+				System.out.println("Welcome " + this.name);
+				System.out.println();
+				System.out.println("1. View Profile");
+				System.out.println("2. View Courses");
+				System.out.println("3. Enroll/Drop a student");
+				System.out.println("4. View Report");
+				System.out.println("5. Logout");
+				System.out.print("Enter Choice: ");				
+
+				int option = sc.nextInt();
+				System.out.println();
+				
+				while(true){
+					if(option==1){
+						viewProfile();
+						break;
+					}else if(option==2) {
+						viewCourses();
+						break;
+					}else if(option==3) {
+						enrollordrop();
+						break;
+					}else if(option==4){
+						viewReport();
+						break;
+					}else if(option==5){
+						break;
+					}
+				}				
+				
+			}
 			
 			
 		} catch (SQLException e) {
@@ -21,6 +52,15 @@ public class TeachingAssistant extends Student{
 		}
 	}
 	
+	public void viewProfile() {
+		
+	}
 	
+	public void enrollordrop(){
+		
+	}
 	
+	public void viewReport() {
+		
+	}
 }
